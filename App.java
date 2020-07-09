8cd3ade2bd66f59b65ef2c8adc0ec10ed2829b6c
ERFANIUM <officialerfanshield@gmail.com>
2020-07-10 00:22:33 +0430

    imp exit

--- App.java
+++ App.java
@@ -5,20 +5,30 @@ import java.util.Scanner;
 
 public class App {
   static Scanner scanner;
-  static Account a;
   public static void main(String args[]) {
     App.scanner = new Scanner(System.in);
     App.menu();
   }
 
+
   private static void menu() {
-    System.out.println("-> What Do U want to me?");
-    System.out.println("  1. Create an account for new customer");
-    int n = App.scanner.nextInt();
-    switch (n) {
-      case 1: {
-        App.createAccountForNewCustomer();
-        break;
+    boolean exit = false;
+    while (!exit) {
+      System.out.println("-> What Do U want to me?");
+      System.out.println("  1. Create an account for new customer");
+      System.out.println("  0. Exit app");
+
+      int n = App.scanner.nextInt();
+      switch (n) {
+        case 1: {
+          App.createAccountForNewCustomer();
+          break;
+        }
+        case 0: {
+          exit = true;
+          System.out.println("Bye!");
+          break;
+        }
       }
     }
   }
@@ -27,11 +37,16 @@ public class App {
     System.out.println("-> What type of account do U want?");
     System.out.println("  1. Transaction account");
     System.out.println("  2. Saving account");
+    System.out.println("  0. Exit");
+
 
     int n = App.scanner.nextInt();
     switch (n) {
       case 1: {
         App.createAccountForNewCustomer();
+        break;
+      }
+      case 0: {
         break;
       }
     }
